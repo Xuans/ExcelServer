f3af24b7e144cb126cdf502bf6bf2dc9e29b863d
anrainie <anrainie@163.com>
2019-05-29 09:55:26 +0800

    excel filter

--- excel.js
+++ excel.js
@@ -1,4 +1,3 @@
-
 const excel = require('excel').default;
 const fs = require('fs');
 
@@ -14,19 +13,40 @@ router.get('/excel/get', async (ctx, next) => {
     try {
         const filename = `${ctx.request.query.filename}.xlsx`;
         const filepath = path.resolve(process.cwd(), path.join(CONFIG.DATASOURCE, filename));
+        const filter = ctx.request.query.filter;
 
         if (fs.existsSync(filepath)) {
-            const timeout = setTimeout(() => Result.error(ctx, { message: '读取文件超时' }), CONFIG.TIMEOUT);
+            const timeout = setTimeout(() => Result.error(ctx, {
+                message: '读取文件超时'
+            }), CONFIG.TIMEOUT);
 
             const content = await excel(filepath);
 
+            if (filter && content && content.constructor == Array) {
+                try {
+                    let _content = [];
+                    for (let i = 0; i < content.length; i++) {
+                        let x = eval(`(${filter})(${content[i]},${i})`);
+                        if (x != false) {
+                            _content.push(x);
+                        }
+                    }
+                } catch (e) {
+                    console.error(e);
+                }
+            }
+
             clearTimeout(timeout);
 
-            Result.success(ctx, { content });
+            Result.success(ctx, {
+                content
+            });
 
 
         } else {
-            Result.error(ctx, { message: '找不到文件' });
+            Result.error(ctx, {
+                message: '找不到文件'
+            });
         }
     } catch (e) {
         Result.error(ctx, e)
